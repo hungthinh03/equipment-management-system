ccbab8620bbec30a229e6498f284cb94fdc93ee3
thinh.nguyendh <nguyenduchungthinh03@gmail.com>
2025-09-29 17:17:18 +0700

    New X-Service-Source header for requests

--- api-gateway/src/main/java/com/example/gateway/config/GatewayConfig.java
+++ api-gateway/src/main/java/com/example/gateway/config/GatewayConfig.java
@@ -15,9 +15,19 @@ public class GatewayConfig {
         return builder.routes()
                 .route("auth-service", r -> r.path("/auth/**")
                         .uri("http://localhost:8082"))  // no JWT needed
-                .route("device-service", r -> r.path("/device/**")
+                .route("device-service-external-request", r -> r.path("/device/**")
+                        .and().header("X-Service-Source", "request-service") // if is from Request Service
+                        .and().not(h -> h.header("X-Processed-Route", "true"))
                         .filters(f -> f
-                                .removeRequestHeader("X-*") // strip client's potential custom headers
+                                .removeRequestHeader("X-*")
+                                .filter(jwtAuthFilter.apply(new JwtAuthFilter.Config()))
+                                .addRequestHeader("X-Service-Source", "request-service")
+                                .addRequestHeader("X-Processed-Route", "true")
+                        )
+                        .uri("http://localhost:8083"))
+                .route("device-service", r -> r.path("/device/internal/**")
+                        .filters(f -> f
+                                .removeRequestHeader("X-*") // strip client's custom headers
                                 .filter(jwtAuthFilter.apply(new JwtAuthFilter.Config())) // inject safe headers
                         )
                         .uri("http://localhost:8083")) // JWT required
