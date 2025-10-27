42538469a9e2819a9c876a70cee2f4265e7a4319
thinh.nguyendh <nguyenduchungthinh03@gmail.com>
2025-10-27 13:50:34 +0700

    Routing within docker container

--- api-gateway/src/main/java/com/example/gateway/config/GatewayConfig.java
+++ api-gateway/src/main/java/com/example/gateway/config/GatewayConfig.java
@@ -15,7 +15,7 @@ public class GatewayConfig {
     public RouteLocator customRoutes(RouteLocatorBuilder builder, JwtAuthFilter jwtAuthFilter) {
         return builder.routes()
                 .route("auth-service", r -> r.path("/auth/**")
-                        .uri("http://localhost:8082"))  // Replace with localhost for local run
+                        .uri("http://auth-app:8082"))  // Replace with localhost for local run
                 .route("device-service-external-request", r -> r.path("/device/**")
                         .and().header("X-Service-Source", ".*") // if is from another service
                         .and().not(h -> h.header("X-Processed-Route", "true"))
@@ -38,26 +38,26 @@ public class GatewayConfig {
                                             .build());
                                 })
                         )
-                        .uri("http://localhost:8083"))
+                        .uri("http://device-app:8083"))
                 .route("device-service", r -> r.path("/device/**")
                         .filters(f -> f
                                 .removeRequestHeader("X-*") // strip client's custom headers
                                 .filter(jwtAuthFilter.apply(new JwtAuthFilter.Config())) // inject safe headers
                         )
-                        .uri("http://localhost:8083")) // JWT required
+                        .uri("http://device-app:8083"))
                 .route("request-service", r -> r.path("/request/**")
                         .filters(f -> f
                                 .removeRequestHeader("X-*")
                                 .filter(jwtAuthFilter.apply(new JwtAuthFilter.Config()))  // JWT required
                         )
-                        .uri("http://localhost:8084"))
+                        .uri("http://request-app:8084"))
                 .route("report-service", r -> r.path("/report/**")
                         .filters(f -> f
                                 .removeRequestHeader("X-*")
                                 .filter(jwtAuthFilter.apply(new JwtAuthFilter.Config()))  // JWT required
                                 .filter(new RoleFilter("ADMIN"))
                         )
-                        .uri("http://localhost:8085"))
+                        .uri("http://report-app:8085"))
                 .build();
     }
 }
